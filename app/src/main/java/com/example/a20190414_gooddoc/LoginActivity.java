6671fe2535c4e027196975221f0e088ed5d8452a
jjysh1 <jjyshj@kcpmed.com>
2019-04-14 15:17:08 +0900

    로그인 실패시 토스트

--- app/src/main/java/com/example/a20190414_gooddoc/LoginActivity.java
+++ app/src/main/java/com/example/a20190414_gooddoc/LoginActivity.java
@@ -2,12 +2,44 @@ package com.example.a20190414_gooddoc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+//ID : tjeit,  PW : qwer1234
+// 로그인되면 메인화면 열고
+//    아니면 토스트로 로그인 실패 메세지
+
+    EditText txtid;
+    EditText txtpw;
+    Button btnlogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        txtid = findViewById(R.id.txtid);
+        txtpw = findViewById(R.id.txtpw);
+        btnlogin = findViewById(R.id.btnlogin);
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userId = txtid.getText().toString();
+                String userPw = txtpw.getText().toString();
+
+                if (userId.equals("tjeit") && userPw.equals("qwer1234")){
+
+                }
+                else{
+                    Toast.makeText(LoginActivity.this, "로그인에 실패했습니다. ID/PW를 확인하세요.", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
     }
 }
